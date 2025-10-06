1bdf6c6160850a8c30b03c61c37ea685a0a2bb11
Vincent Xu <vincent.xu1@outlook.com>
2025-10-06 13:16:50 -0700

    Added derived dbContext class

---
+++ Logs.Api/Data/LogsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Logs.Api.Models;
+
+namespace Logs.Api.Data
+{
+    public class LogsContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+
+        public LogsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
