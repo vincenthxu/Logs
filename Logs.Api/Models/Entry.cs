f70b5f04f70368dd0f6712c68c302aaa3d8258b9
Vincent Xu <vincent.xu1@outlook.com>
2025-10-06 13:16:32 -0700

    Added User and Entry model classes

---
+++ Logs.Api/Models/Entry.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Logs.Api.Models
+{
+    // Reference: https://doi.org/10.3109/00365529709011203
+    public enum BristolStoolScale
+    {
+        Type1 = 1, Type2, Type3, Type4, Type5, Type6, Type7
+    }
+    public class Entry
+    {
+        #region Constructors
+        public Entry(Guid userId, DateOnly date, TimeOnly time, BristolStoolScale bristolStoolScale)
+        {
+            UserId = userId;
+            Date = date;
+            Time = time;
+            BristolStoolScale = bristolStoolScale;
+        }
+        #endregion
+
+        #region Properties
+        public int Id { get; set; }
+        public Guid UserId { get; private set; }
+        [JsonIgnore]
+        public virtual User User { get; set; } // `virtual` enables lazy loading and establishes UserId as a foreign key
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public BristolStoolScale BristolStoolScale { get; set; }
+        public string? Notes { get; set; } = null;
+        #endregion
+
+        #region Methods
+        public override string ToString() => System.Text.Json.JsonSerializer.Serialize(this);
+        #endregion
+    }
+}
---
+++ Logs.Api/Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace Logs.Api.Models
+{
+    public class User
+    {
+        #region Constructors
+        public User(string name, DateOnly dateOfBirth)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
+        #endregion
+
+        #region Properties
+        public Guid Id { get; private set; }
+        public string? Name { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public int Age
+        {
+            get
+            {
+                int years = DateTime.Today.Year - DateOfBirth.Year;
+                return DateOnly.FromDateTime(DateTime.Today) > DateOfBirth.AddYears(years) ? years : --years;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString() => System.Text.Json.JsonSerializer.Serialize(this);
+        #endregion
+    }
+}
