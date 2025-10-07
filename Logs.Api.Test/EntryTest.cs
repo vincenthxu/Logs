616e9a97d0d66f278e72d90b5a1a5f34c8585319
Vincent Xu <vincent.xu1@outlook.com>
2025-10-07 13:59:02 -0700

    Added unit tests for Entry class

---
+++ Logs.Api.Test/EntryTest.cs
@@ -0,0 +1,43 @@
+﻿using Logs.Api.Models;
+using System.Text.Json;
+
+namespace Logs.Api.Test;
+
+public class EntryTest
+{
+    private User user;
+    private Entry entry;
+    [SetUp]
+    public void Setup()
+    {
+        user = new(name: "test", dateOfBirth: DateOnly.FromDateTime(DateTime.Today));
+        entry = new(
+            userId: user.Id,
+            date: DateOnly.FromDateTime(DateTime.Today),
+            time: TimeOnly.FromDateTime(DateTime.Now),
+            bristolStoolScale: BristolStoolScale.Type4
+        );
+        entry.User = user;
+    }
+
+    [Test]
+    public void SetId_NewId_SetsCorrectly()
+    {
+        int id = 420;
+        Assert.That(entry.Id, Is.Not.EqualTo(id));
+
+        entry.Id = id;
+        Assert.That(entry.Id, Is.EqualTo(id));
+    }
+
+    [Test]
+    public void ToString_OnInstance_ReturnsJsonSerializedString()
+    {
+        var deserializedObject = JsonSerializer.Deserialize<Entry>(entry.ToString());
+        Assert.That(deserializedObject, Is.TypeOf(typeof(Entry)));
+        Assert.That(deserializedObject.Id, Is.EqualTo(entry.Id));
+        Assert.That(deserializedObject.UserId, Is.EqualTo(entry.UserId));
+        Assert.That(deserializedObject.Date, Is.EqualTo(entry.Date));
+        Assert.That(deserializedObject.Time, Is.EqualTo(entry.Time));
+    }
+}
