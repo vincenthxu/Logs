26028c0c5d603a9f5d8044544360a1ba5e0b3218
Vincent Xu <vincent.xu1@outlook.com>
2025-10-21 23:37:29 -0700

    Added logout endpoint

--- Logs.Api/Program.cs
+++ Logs.Api/Program.cs
@@ -1,5 +1,6 @@
 using Logs.Api.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Logs.Api
@@ -53,6 +54,19 @@ namespace Logs.Api
 
             app.UseCors();
             app.MapIdentityApi<IdentityUser>();
+
+            // Add logout endpoint
+            app.MapPost("/logout", async (SignInManager<IdentityUser> signInManager, [FromBody] object empty) =>
+            {
+                if (empty != null)
+                {
+                    await signInManager.SignOutAsync();
+                    return Results.Ok();
+                }
+                return Results.Unauthorized();
+            })
+            .WithOpenApi();
+
 
             using (var scope = app.Services.CreateScope())
             {
